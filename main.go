e4ba39e1c8ceea4b7175e15e7305e286c6f6335f
Logan Sichelstiel <logansichelstiel@gmail.com>
2026-07-22 19:12:16 -0400

    Fix bug in third zone formula

--- main.go
+++ main.go
@@ -203,7 +203,7 @@ func calculateGermanyTax(income float64) {
 		germanyTax = (914.51*scaledIncome + 1400) * scaledIncome // formula for the second zone of incomes
 	} else if euros <= 69878 {
 		scaledIncome = (euros - 17799) / 10000
-		germanyTax = (173.10*scaledIncome + 2397) + 1034.87 // third zone
+		germanyTax = (173.10*scaledIncome+2397)*scaledIncome + 1034.87 // third zone
 	} else if euros <= 277825 {
 		germanyTax = 0.42*(euros-69878) + 14414.87 // fourth zone
 	} else {
